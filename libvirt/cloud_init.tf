198cde724431d6ad09762844d20ae36550102293
amaelcattaneo@gmail.com <amaelcattaneo@gmail.com>
2020-04-20 15:49:24 +0200

    adding informations

--- libvirt/cloud_init.tf
+++ libvirt/cloud_init.tf
@@ -1,4 +1,4 @@
-# # read the cloud init configuration
+# read the cloud init configuration
 # data "template_file" "ubuntu_user_data" {
 #   template = "${file("./confs/ubuntu_cloud_init.yml")}"
 # }
@@ -10,12 +10,32 @@
 #   user_data = "${data.template_file.ubuntu_user_data.rendered}"
 # }
 
-# data "template_file" "centos_user_data" {
-#   template = "${file("./confs/centos_cloud_init.yml")}"
+data "template_file" "centos_user_data" {
+  template = "${file("./confs/centos_cloud_init.yml")}"
+}
+
+resource "libvirt_cloudinit_disk" "centos_cloudinitiso" {
+  name      = "centos_cloud.iso"
+  pool      = "${libvirt_pool.vm-pool.name}"
+  user_data = "${data.template_file.centos_user_data.rendered}"
+}
+
+# data "template_file" "windows_user_data" {
+#   template = "${file("./confs/windows_cloud_init.yml")}"
 # }
 
-# resource "libvirt_cloudinit_disk" "centos_cloudinitiso" {
-#   name      = "centos_cloud.iso"
+# resource "libvirt_cloudinit_disk" "windows_cloudinitiso" {
+#   name      = "windows_cloud.iso"
 #   pool      = "${libvirt_pool.vm-pool.name}"
-#   user_data = "${data.template_file.centos_user_data.rendered}"
+#   user_data = "${data.template_file.windows_user_data.rendered}"
+# }
+
+# data "template_file" "pfsense_user_data" {
+#   template = "${file("./confs/pfsense_cloud_init.yml")}"
+# }
+
+# resource "libvirt_cloudinit_disk" "pfsense_cloudinitiso" {
+#   name      = "pfsense_cloud.iso"
+#   pool      = "${libvirt_pool.vm-pool.name}"
+#   user_data = "${data.template_file.pfsense_user_data.rendered}"
 # }
