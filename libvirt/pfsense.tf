f37f653a63c3fc80451a072d55c960e5ddd68825
amaelcattaneo@gmail.com <amaelcattaneo@gmail.com>
2020-04-20 15:49:31 +0200

    adding informations

---
+++ libvirt/pfsense.tf
@@ -0,0 +1,37 @@
+# create a libvirt image
+resource "libvirt_volume" "pfsense" {
+  name   = "pfsense.qcow2"
+  pool   = "${libvirt_pool.vm-pool.name}"
+  source = "${var.pfsense_img}"
+  format = "qcow2"
+}
+
+# Create a root volume (pour agrandir le disk)
+resource "libvirt_volume" "pfsense_rootfs" {
+  name = "rootfs"
+  base_volume_id = "${libvirt_volume.pfsense.id}"
+  size = "11000000000"
+}
+
+# Create the machine
+resource "libvirt_domain" "pfsense" {
+  name   = "pfsense"
+  memory = "2048"
+  vcpu   = 4
+
+  cloudinit = "${libvirt_cloudinit_disk.pfsense_cloudinitiso.id}"
+
+  network_interface {
+    network_name = "default"
+  }
+
+  disk {
+    volume_id = "${libvirt_volume.pfsense_rootfs.id}"
+  }
+
+  graphics {
+    type        = "spice"
+    listen_type = "address"
+    autoport    = true
+  }
+}
